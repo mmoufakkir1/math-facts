906e55e08d382b081f35c2ed32c1f0f800a154b4
mmoufakkir1 <mmoufakkir1@yahoo.com>
2020-04-15 17:45:37 -0400

    removed redux and used state, refact some compoments

--- src/actions/main.js
+++ src/actions/main.js
@@ -1,57 +1,8 @@
-export const HEADER_STATE = 'HEADER_STATE';
-export const  OPERATION_STATE ='OPERATION_STATE';
-export const   RESULT_STATE = 'RESULT_STATE;'
-export const EQUATION_COUNT_STATE= 'EQUATION_COUNT_STATE';
 export const EQUATIONS_STATE= 'EQUATIONS_STATE';
-export const MODAL_EQUATION_STATE='MODAL_EQUATION_STATE';
-export const STEPPER_STATE = 'STEPPER_STATE';
-
-
-export function updateStepperState(step) {
-    return {
-        type: STEPPER_STATE,
-        payload: step,
-    }
-}
-
-export function updateHeaderState(header) {
-    return {
-        type: HEADER_STATE,
-        payload: header,
-    }
-}
-
-export function updateOperationState(operation) {
-    return {
-        type: OPERATION_STATE,
-        payload: operation,
-    }
-}
-
-export function updateResultState(result) {
-    return {
-        type: RESULT_STATE,
-        payload: result,
-    }
-}
-
-export function updateEquationCountState(count) {
-    return {
-        type: EQUATION_COUNT_STATE,
-        payload: count,
-    }
-}
 
 export function updateEquationsState(equations) {
     return {
         type: EQUATIONS_STATE,
         payload: equations,
-    }
-}
-
-export function updateModalEquationsState(open) {
-    return {
-        type: MODAL_EQUATION_STATE,
-        payload: open,
     }
 }
--- src/reducers/main.js
+++ src/reducers/main.js
@@ -1,144 +1,11 @@
 import { combineReducers } from 'redux';
 import {
-  HEADER_STATE,
-  OPERATION_STATE,
-  RESULT_STATE,
-  EQUATION_COUNT_STATE,
   EQUATIONS_STATE,
-  MODAL_EQUATION_STATE,
-  STEPPER_STATE
 } from '../actions/main';
 
-export const initialHeaderState = {
-  header: 'Math Facts'
-}
-
-export const initialOperationState = {
-  operation: ''
-}
-
-export const initialResultState = {
-  result: ''
-}
-
-export const initialEquationCountState = {
-  count: 10
-}
 
 export const initialEquationsState = {
   equations: []
-}
-
-
-export const initialModalEquationsState = {
-  open: false
-}
-
-
-export const initialStepperState = {
-  step: 0
-}
-
-const getStepper = (state = initialStepperState, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-
-    case STEPPER_STATE:
-      {
-        return {
-          ...state,
-          step: payload,
-        }
-      }
-
-    default:
-      {
-        return state;
-      }
-  }
-}
-
-const getHeader = (state = initialHeaderState, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-
-    case HEADER_STATE:
-      {
-        return {
-          ...state,
-          header: payload,
-        }
-      }
-
-    default:
-      {
-        return state;
-      }
-  }
-}
-
-
-const getResult = (state = initialResultState, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-
-    case RESULT_STATE:
-      {
-        return {
-          ...state,
-          result: payload,
-        }
-      }
-
-    default:
-      {
-        return state;
-      }
-  }
-}
-
-const getOperation = (state = initialOperationState, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-
-    case OPERATION_STATE:
-      {
-        return {
-          ...state,
-          operation: payload,
-        }
-      }
-
-    default:
-      {
-        return state;
-      }
-  }
-}
-
-
-const getEquationCount = (state = initialEquationCountState, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-
-    case EQUATION_COUNT_STATE:
-      {
-        return {
-          ...state,
-          count: payload,
-        }
-      }
-
-    default:
-      {
-        return state;
-      }
-  }
 }
 
 
@@ -162,33 +29,7 @@ const getEquations = (state = initialEquationsState, action) => {
   }
 }
 
-const getModalEquations = (state = initialModalEquationsState, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-
-    case MODAL_EQUATION_STATE:
-      {
-        return {
-          ...state,
-          open: payload,
-        }
-      }
-
-    default:
-      {
-        return state;
-      }
-  }
-}
-
 const main = combineReducers({
-  getHeader,
-  getOperation,
-  getResult,
-  getEquationCount,
-  getEquations,
-  getModalEquations,
-  getStepper
+  getEquations
 });
 export default main;
